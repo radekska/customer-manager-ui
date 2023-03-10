f31cff27633fe42eaf7a022af21b15f3f7c4e667
radekska <r.skalbania@piwik.pro>
2023-03-10 19:49:39 +0100

    clear things

--- src/store/store.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import currentCustomerReducer from '../reducers/dummyReducer'
-
-export default configureStore({
-  reducer: {currentCustomerReducer},
-})
