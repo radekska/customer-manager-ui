dd7b27b1459b2e1ce0968ee1c66e3c55868f46df
radekska <r.skalbania@piwik.pro>
2023-06-02 21:59:52 +0200

    set proper api url

--- src/redux/reducers/purchases.ts
+++ src/redux/reducers/purchases.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 import {Purchase} from "../../models/purchase";
 
+const origin = window.location.origin
+
 export function listPurchases(customerId: string) {
     return async function listPurchasesThunk(dispatch: any, getState: any) {
         dispatch({type: "purchases/purchasesLoading"})
-        axios.get<Purchase[]>(`${process.env.REACT_APP_CUSTOMER_MANAGER_API}/api/customers/${customerId}/purchases`).then(response => {
+        axios.get<Purchase[]>(`${origin}/api/customers/${customerId}/purchases`).then(response => {
             dispatch({
                 type: "purchases/purchasesLoaded",
                 payload: response.data
@@ -16,7 +18,7 @@ export function listPurchases(customerId: string) {
 export function addPurchase(customerId: string, frameModel: string, lensPower: string, lensType: string, pd: string, purchaseType: string, purchasedAt: string) {
     return async function addPurchaseThunk(dispatch: any, getState: any) {
         dispatch({type: "purchases/purchaseAdding"})
-        axios.post<Purchase>(`${process.env.REACT_APP_CUSTOMER_MANAGER_API}/api/customers/${customerId}/purchases`, {
+        axios.post<Purchase>(`${origin}/api/customers/${customerId}/purchases`, {
             frame_model: frameModel,
             lens_power: lensPower,
             lens_type: lensType,
@@ -47,7 +49,7 @@ export function addPurchase(customerId: string, frameModel: string, lensPower: s
 export function deletePurchase(customerId: string, purchaseId: string) {
     return async function deletePurchaseThunk(dispatch: any, getState: any) {
         dispatch({type: "purchases/purchaseDeleting"})
-        axios.delete<Purchase>(`${process.env.REACT_APP_CUSTOMER_MANAGER_API}/api/customers/${customerId}/purchases/${purchaseId}`).then(response => {
+        axios.delete<Purchase>(`${origin}/api/customers/${customerId}/purchases/${purchaseId}`).then(response => {
             dispatch({
                 type: "purchases/purchaseDeletingSuccess",
                 payload: purchaseId
